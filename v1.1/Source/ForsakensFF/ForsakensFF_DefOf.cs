4be1f7c41171f9569019e4a41044adebf05b5691
juanosarg <31105173+juanosarg@users.noreply.github.com>
2020-02-22 15:57:51 +0100

    v2.0

---
+++ v1.1/Source/ForsakensFF/ForsakensFF_DefOf.cs
@@ -0,0 +1,18 @@
+﻿using RimWorld;
+using System;
+using Verse;
+
+namespace ForsakensFF
+{
+    [DefOf]
+    public static class ForsakensFF_DefOf
+    {
+        public static ThingDef FO_RBehemoth;
+
+        public static PawnKindDef FO_Behemoth;
+
+        public static PawnKindDef FO_Murkling;
+
+        public static WeatherDef DryThunderstorm;
+    }
+}
